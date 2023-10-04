4345be70173eb00694b898ce62711cbbab16e39a
adiwira09 <61595879+adiwira09@users.noreply.github.com>
2023-10-04 09:48:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import filedialog
+import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -42,72 +41,30 @@ class DataRegistration:
         elif gender == 'Wanita':
             self.driver.find_element(By.ID, 'rbOption2').click()
 
-class DataRegistrationGUI:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Registrasi Data")
-        self.master.geometry("300x210")
+def main():
+    st.title("Registrasi Data")
 
-        self.label1 = tk.Label(master, text="Nama File:")
-        self.label1.pack()
-
-        self.file_entry = tk.Entry(master)
-        self.file_entry.pack()
-
-        self.browse_button = tk.Button(master, text="Pilih File", command=self.browse_file)
-        self.browse_button.pack()
-
-        self.row_label = tk.Label(master, text="Baris ke:")
-        self.row_label.pack()
-
-        self.row_entry = tk.Entry(master)
-        self.row_entry.pack()
-
-        self.data_label = tk.Label(master, text="Banyak Data:")
-        self.data_label.pack()
-
-        self.data_entry = tk.Entry(master)
-        self.data_entry.pack()
-
-        self.submit_button = tk.Button(master, text="Mulai Registrasi", command=self.start_registration)
-        self.submit_button.pack()
-
-        self.add_creator_label()
-
-    def add_creator_label(self):
-        creator_label = tk.Label(self.master, text="by Nugroho Adi")
-        creator_label.pack(anchor='nw', side='bottom')
-
-    def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
-        self.file_entry.delete(0, tk.END)
-        self.file_entry.insert(0, file_path)
-
-    def start_registration(self):
-        file_path = self.file_entry.get()
-        row = int(self.row_entry.get())
-        banyak_data = int(self.data_entry.get())
+    file_path = st.file_uploader("Pilih File Excel", type=["xlsx"])
+    if file_path is not None:
+        row = st.number_input("Baris ke:", min_value=1, step=1, value=1)
+        banyak_data = st.number_input("Banyak Data:", min_value=1, step=1, value=1)
 
         registration = DataRegistration(file_path)
         sheet_range = registration.load_data()
 
-        for i in range(row+1, banyak_data+row+1):
-            data = {
-                'name': sheet_range['E'+str(i)].value,
-                'identity_number' : '-',
-                'npwp_number' : '-',
-                'email': sheet_range['H'+str(i)].value,
-                'phone_number': f"0{sheet_range['I'+str(i)].value}",
-                'birth_year' :'2003',
-                'address' : 'Depok',
-                'gender': 'Pria' if i % 2 == 0 else 'Wanita'
-            }
-            registration.register_data(data)
-
-def main():
-    root = tk.Tk()
-    app = DataRegistrationGUI(root)
-    root.mainloop()
+        if st.button("Mulai Registrasi"):
+            for i in range(row+1, banyak_data+row+1):
+                data = {
+                    'name': sheet_range['E'+str(i)].value,
+                    'identity_number': '-',
+                    'npwp_number': '-',
+                    'email': sheet_range['H'+str(i)].value,
+                    'phone_number': f"0{sheet_range['I'+str(i)].value}",
+                    'birth_year': '2003',
+                    'address': 'Depok',
+                    'gender': 'Pria' if i % 2 == 0 else 'Wanita'
+                }
+                registration.register_data(data)
 
 if __name__ == '__main__':
     main()
