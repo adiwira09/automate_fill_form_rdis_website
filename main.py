eebadb42848843ddb2a508b44cf67c7d97c20f43
adiwira09 <61595879+adiwira09@users.noreply.github.com>
2024-03-18 17:57:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,13 @@ class DataRegistration:
         self.driver.get('https://rdis.idx.co.id/id/register-visit-onsite/390')
 
         name = data['name']
-        identity_number = '-'
-        npwp_number = '-'
         email = data['email']
         phone_number = data['phone_number']
         birth_year = '2004'
         address = 'Depok'
         gender = data['gender']
 
-        self.driver.find_element(By.NAME, 'identity_number').send_keys(identity_number)
         self.driver.find_element(By.NAME, 'name').send_keys(name)
-        self.driver.find_element(By.NAME, 'npwp_number').send_keys(npwp_number)
         self.driver.find_element(By.NAME, 'email').send_keys(email)
         self.driver.find_element(By.NAME, 'phone_number').send_keys(phone_number)
         select = Select(self.driver.find_element(By.NAME, 'birth_year'))
@@ -41,42 +37,39 @@ class DataRegistration:
             self.driver.find_element(By.ID, 'rbOption1').click()
         elif gender == 'Wanita':
             self.driver.find_element(By.ID, 'rbOption2').click()
+        self.driver.find_element(By.XPATH, '//*[@id="recaptcha-element"]/div/div/iframe').click()
 
 class DataRegistrationGUI:
     def __init__(self, master):
         self.master = master
         self.master.title("Registrasi Data")
-        self.master.geometry("300x210")
+        self.master.geometry("350x170")
 
-        self.label1 = tk.Label(master, text="Nama File:")
-        self.label1.pack()
+        self.label_nama = tk.Label(master, text="Nama File", font=("Calibri", 15))
+        self.label_baris = tk.Label(master, text="Baris ke", font=("Calibri", 15))
+        self.label_data = tk.Label(master, text="Banyak Data", font=("Calibri", 15))
 
         self.file_entry = tk.Entry(master)
-        self.file_entry.pack()
-
-        self.browse_button = tk.Button(master, text="Pilih File", command=self.browse_file)
-        self.browse_button.pack()
-
-        self.row_label = tk.Label(master, text="Baris ke:")
-        self.row_label.pack()
-
         self.row_entry = tk.Entry(master)
-        self.row_entry.pack()
-
-        self.data_label = tk.Label(master, text="Banyak Data:")
-        self.data_label.pack()
-
         self.data_entry = tk.Entry(master)
-        self.data_entry.pack()
 
-        self.submit_button = tk.Button(master, text="Mulai Registrasi", command=self.start_registration)
-        self.submit_button.pack()
+        self.browse_button = tk.Button(master, text="Pilih File", command=self.browse_file, font=("Verdana", 12))
+        self.submit_button = tk.Button(master, text="Mulai Registrasi", command=self.start_registration, font=("Verdana", 12))
 
-        self.add_creator_label()
+        self.label_nama.grid(row=1, column=1, sticky = 'W')
+        self.label_baris.grid(row=2, column=1, sticky = 'W')
+        self.label_data.grid(row=3, column=1, sticky = 'W')
 
-    def add_creator_label(self):
+        self.file_entry.grid(row=1, column=2, ipady = 3, ipadx = 5)
+        self.row_entry.grid(row=2, column=2, ipady = 3, ipadx = 5)
+        self.data_entry.grid(row=3, column=2, ipady = 3, ipadx = 5)
+
+        self.browse_button.grid(row=1, column=3)
+        self.submit_button.grid(row=4, column=2, pady=10)
+
+    # def add_creator_label(self):
         creator_label = tk.Label(self.master, text="by Nugroho Adi")
-        creator_label.pack(anchor='nw', side='bottom')
+        creator_label.grid(row=5, column=1)
 
     def browse_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
@@ -106,7 +99,7 @@ class DataRegistrationGUI:
 
 def main():
     root = tk.Tk()
-    app = DataRegistrationGUI(root)
+    DataRegistrationGUI(root)
     root.mainloop()
 
 if __name__ == '__main__':
